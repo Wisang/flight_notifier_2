f8c526961ac522d6f35f1e62f4b3444c8e60f926
wisang <wseom7@gmail.com>
2022-02-22 17:05:03 +0900

    works but need to be refactored

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ FLIGHT_SEARCH_KEY = "lAwUkoUL72fXEVrFROaWQoHMgfH0xTH8"
 flight_search_endpoint = "https://tequila-api.kiwi.com/v2/search"
 
 account_sid = "AC5d5d1d3bed511b084991b20279367956"
-auth_token = "ce8420ecfcf11bab5faf048c0083fffd"
+auth_token = "ed5c8d1fef5c6be30de6f9c36bb8a6ca"
 
 sheety_header = {
     "Authorization": SHEETY_TOKEN
@@ -19,31 +19,36 @@ flight_header = {
     "apikey": FLIGHT_SEARCH_KEY,
 }
 
-flight_body = {
-    "fly_from": "LGA",
-    "fly_to": "MIA",
-    "dateFrom": "22/02/2022",
-    "dateTo": "23/02/2022",
-}
+# flight_body = {
+#     "fly_from": "ICN",
+#     "fly_to": "MIA",
+#     "dateFrom": "22/02/2022",
+#     "dateTo": "23/02/2022",
+# }
 
 response = requests.get(url=sheety_endpoint, headers=sheety_header)
-# print(response.json())
+price_data = response.json()["prices"]
 
-response = requests.get(url=flight_search_endpoint, headers=flight_header, params=flight_body)
+city_lowest_price = {item["iataCode"]: item["lowestPrice"]
+                     for item in price_data}
 
-data_list = response.json()["data"]
+print(city_lowest_price)
 
-price_list = [{item["cityTo"]: int(item["price"])} for item in data_list]
-
-print(price_list)
-
-print(str(price_list))
-
-if True:  # need_to_bring_umbrella:
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body=str(price_list),
-        from_='+18596952590',
-        to='+821023309854'
-    )
-    print(message.status)
+for key in city_lowest_price:
+    flight_body = {
+        "fly_from": "ICN",
+        "fly_to": key,
+        "dateFrom": "22/02/2022",
+        "dateTo": "23/02/2022",
+    }
+    response = requests.get(url=flight_search_endpoint, headers=flight_header, params=flight_body)
+    data_list = response.json()["data"]
+    for item in data_list:
+        if city_lowest_price[key] > int(item["price"]):
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                body=f"to {item['cityTo']} is ${item['price']} on {item['route'][0]['last_seen']}",
+                from_='+18596952590',
+                to='+821023309854'
+            )
+            print(message.status)
